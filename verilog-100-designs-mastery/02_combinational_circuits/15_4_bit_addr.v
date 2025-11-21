aabd42a1a50d6c8a174e071057dd6a18c305b539
Brahma Ganesh katrapalli <146933290+BrahmaGanesh@users.noreply.github.com>
2025-11-21 15:49:57 +0530

    Fix carry wire connections in 4-bit adder

--- verilog-100-designs-mastery/02_combinational_circuits/15_4_bit_addr.v
+++ verilog-100-designs-mastery/02_combinational_circuits/15_4_bit_addr.v
@@ -7,9 +7,9 @@ module adder_4bit(
 );
     wire c1,c2,c3;
 
-    full_adder f1 (.A(a[0]),.B(b[0]),.Cin(cin),.f_sum(a_sum[0]),.f_carry(b1));
-    full_adder f2 (.A(a[1]),.B(b[1]),.Cin(b1),.f_sum(a_sum[1]),.f_carry(b2));
-    full_adder f3 (.A(a[2]),.B(b[2]),.Cin(b2),.f_sum(a_sum[2]),.f_carry(b3));
-    full_adder f4 (.A(a[3]),.B(b[3]),.Cin(b3),.f_sum(a_sum[3]),.f_carry(a_carry));
+    full_adder f1 (.A(a[0]),.B(b[0]),.Cin(cin),.f_sum(a_sum[0]),.f_carry(c1));
+    full_adder f2 (.A(a[1]),.B(b[1]),.Cin(c1),.f_sum(a_sum[1]),.f_carry(c2));
+    full_adder f3 (.A(a[2]),.B(b[2]),.Cin(c2),.f_sum(a_sum[2]),.f_carry(c3));
+    full_adder f4 (.A(a[3]),.B(b[3]),.Cin(c3),.f_sum(a_sum[3]),.f_carry(a_carry));
 
 endmodule
