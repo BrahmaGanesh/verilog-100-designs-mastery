0161143684fb49f813f920c7d4c9d2374e83359e
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2025-11-26 15:09:17 +0530

    4-bit comparator design

---
+++ verilog-100-designs-mastery/05_comparators_parity/43_4_bit_comparator.v
@@ -0,0 +1,13 @@
+module bit_comparator_2(
+    input  wire [3:0] a,
+    input  wire [3:0] b,
+    output wire Y_equal,
+    output wire Y_less,
+    output wire Y_greater
+);
+
+    assign Y_equal   = (a == b);
+    assign Y_less    = (a < b);
+    assign Y_greater = (a > b);
+
+endmodule
