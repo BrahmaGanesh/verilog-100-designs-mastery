1c24d4fed865f57984ffc425571785823e3101bf
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2025-12-01 21:47:06 +0530

    Added SR Latch module (Design 51): Basic level-sensitive bistable circuit implemented.

--- verilog-100-designs-mastery/06_sequential_circuits/51_sr_latch.v
+++ verilog-100-designs-mastery/06_sequential_circuits/51_sr_latch.v
@@ -5,15 +5,15 @@
     output  wire  Q_bar   
  );
 
- always @(*) begin
-    case({S,R})
-        2'b00 : Q = Q;
-        2'b01 : Q = 1'b0;
-        2'b10 : Q = 1'b1;
-        2'b11 : Q = 1'bx;
-        default : Q = 1'b0;
-    endcase
-end
+    always @(*) begin
+        case({S,R})
+            2'b00 : Q = Q;
+            2'b01 : Q = 1'b0;
+            2'b10 : Q = 1'b1;
+            2'b11 : Q = 1'bx;
+            default : Q = 1'b0;
+        endcase
+    end
 
-assign Q_bar = ~Q;
+    assign Q_bar = ~Q;
 endmodule
